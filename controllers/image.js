0f42a144195d47d9cce6ab15e3eda5f22cd277a2
KLC <mkelsy@icloud.com>
2022-12-08 17:49:56 -0500

    adding api

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai. 
 const app = new Clarifai.App({
- apiKey: 'YOUR API KEY HERE' 
+ apiKey: 'e1633514a197471186699806e157beda' 
 });
 
 
